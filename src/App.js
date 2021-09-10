d5bfc91f5b0144262e5052e267630612619f5c2e
vibes <vibes@vibess-MacBook-Pro.local>
2021-09-10 15:23:02 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [characters, setCharacters] = useState([])
+
+  useEffect( () => {
+
+    axios.get('https://swapi.dev/api/people')
+    .then( data => {
+        console.log(data.data);
+        setCharacters(data.data);
+    })
+
+  },[])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+      characters.map(char => {
+        return <Character key={char.id} info={char}/>
+
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,47 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCharacter = styled.div`
+    width: 80vw;
+    display:flex;
+    margin-top: 10vh;
+    color: #03A062;
+    background-color : #404040;   
+    max-height: 80vh;
+    border-radius: 5px;
+    box-shadow: 0 1px 1px rgba(0,0,0,0.12), 
+              0 2px 2px rgba(0,0,0,0.12), 
+              0 4px 4px rgba(0,0,0,0.12), 
+              0 8px 8px rgba(0,0,0,0.12),
+              0 16px 16px rgba(0,0,0,0.12);
+    padding:40px;
+    justify-content: space-between;
+
+    .planet-picture {
+        overflow: hidden;
+        margin-right: 40px;
+    }
+
+    .planet-description{
+        max-width: 20em;
+        min-width: 20em;
+        overflow: scroll;
+    }
+
+    
+    `
+
+    const Character = props => {
+
+
+        return (
+            <StyledCharacter>
+                <h1>{props.info.name}</h1>
+                <h2>{props.info.birth_year}</h2>
+            </StyledCharacter>
+        )
+
+    }
+
+    export default Character;
